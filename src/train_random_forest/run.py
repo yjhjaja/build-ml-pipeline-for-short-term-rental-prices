7d7893710b9b5737f87b8619e18a491d4f741528
Junhui Yang <119138211+yjhjaja@users.noreply.github.com>
2024-10-04 18:16:13 -0400

    Update run.py

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -159,7 +159,7 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # Build a pipeline with two steps:
     # 1 - A SimpleImputer(strategy='most_frequent') to impute missing values
     # 2 - A OneHotEncoder() step to encode the variable
-    non_ordinal_categorical_preproc = make_pipeline(SimpleImputer(strategy='most_frequent'), OneHotEncoder()) # YOUR CODE HERE
+    non_ordinal_categorical_preproc = make_pipeline(SimpleImputer(strategy='most_frequent'), OneHotEncoder())
     ######################################
 
     # Let's impute the numerical columns to make sure we can handle missing values
